739013c2b79a8b8a2f5400c36ddcfecc5048f66b
Gokdenix <mountainskysea07@gmail.com>
2022-01-23 15:39:48 +0200

    improvemnts on server

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect,url_for
 
 app = Flask(__name__)
-
+@app.route('/')
+def main():
+    return "Welcome"
 @app.route("/<place>")
-def home(place):
-    return "<h1>Welcome {place}</h1>"
+def place(place):
+    return f"<h1>Welcome {place}</h1>"
+@app.route("/admin")
+def admin():
+    return redirect(url_for("place", place="admin"))
 
 if __name__ == '__main__':
     app.run()
