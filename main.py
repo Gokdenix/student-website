3911613476ae73a45168f4172a8e801ecf1772d0
Gokdenix <mountainskysea07@gmail.com>
2022-01-23 15:50:30 +0200

    index.html added with template rendering

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-from flask import Flask, redirect,url_for
+from flask import Flask, redirect,url_for, render_template
 
 app = Flask(__name__)
 @app.route('/')
 def main():
-    return "Welcome"
-@app.route("/<place>")
-def place(place):
-    return f"<h1>Welcome {place}</h1>"
-@app.route("/admin")
-def admin():
-    return redirect(url_for("place", place="admin"))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
