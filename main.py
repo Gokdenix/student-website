a0d3d3db946286472ccdf57a471e513578016e47
Gokdenix <mountainskysea07@gmail.com>
2022-01-23 16:04:34 +0200

    added some new useless features

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, redirect,url_for, render_template
 
 app = Flask(__name__)
-@app.route('/')
-def main():
-    return render_template('index.html')
+@app.route('/<Name>')
+def main(Name):
+    return render_template('index.html', name=Name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
