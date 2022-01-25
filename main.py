7daff422819b7994855dbcfaa7e534f18e4a3a68
Gokdeniz00 <77074479+Gokdeniz00@users.noreply.github.com>
2022-01-25 16:52:54 +0200

    Math webpage initiated

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, redirect,url_for, render_template
 
 app = Flask(__name__)
-@app.route('/')
-def main():
+@app.route('/', methods=['GET','POST'])
+def home(): 
     return render_template('index.html')
-
+@app.route('/Maths', methods=['GET','POST'])
+def maths():
+    return render_template('Maths.html') 
 if __name__ == '__main__':
     app.run(debug=True)
